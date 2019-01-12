483522b49e662625ee28007b45701fd86e7a6381
alienonolympus <alien-on-earth@outlook.com>
2019-01-12 16:04:47 +0800

    Second attempt at fixing SQLite error

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ api = Api(app)
 
 class Database(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    key = db.Column(db.String(120), unique = True, nullable = False)
+    key = db.Column(db.String(120), nullable = False)
     value = db.Column(db.String(120), nullable = False)
 
 db.create_all()
