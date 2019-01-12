2a698bdec4af4b2687fc20f8f99e0007d506ec31
alienonolympus <alien-on-earth@outlook.com>
2019-01-12 14:50:03 +0800

    Added index.html

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template()
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-# initial value
 intro2get = {'data1': 'value1', 'data2': 'value2', 'data3': 'value3'}
 
 class Intro2Get(Resource):
@@ -13,8 +12,11 @@ class Intro2Get(Resource):
 
 class Intro2GetWithId(Resource):
     def get(self, data_name):
-        return {data_name: intro2get[data]}
+        return {data_name: intro2get[data_name]}
 
 api.add_resource(Intro2Get, '/get')
 api.add_resource(Intro2GetWithId, '/get/<string:data_name>')
 
+@app.route('/')
+def index():
+    return render_template('index.html', intro2get=intro2get)
