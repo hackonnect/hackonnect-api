b1a1083803fe1058b867e0701f7ea50ff0feb8e1
alienonolympus <alien-on-earth@outlook.com>
2019-01-12 16:00:29 +0800

    Used SQLAlchemy as database

--- app.py
+++ app.py
@@ -1,40 +1,64 @@
 from flask import Flask, render_template, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
+from flask_sqlalchemy import SQLAlchemy
+import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+db = SQLAlchemy(app)
 api = Api(app)
 
-intro_dict = {'data1': 'value1', 'data2': 'value2', 'data3': 'value3'}
+class Database(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    key = db.Column(db.String(120), unique = True, nullable = False)
+    value = db.Column(db.String(120), nullable = False)
 
-class Intro2Get(Resource):
+db.create_all()
+for i in range(3):
+    db.session.add(Database(key = 'data' + str(i + 1), value = 'value' + str(i + 1)))
+db.session.commit()
+
+class GetAll(Resource):
     def get(self):
-        return intro_dict
+        data_all = Database.query.all()
+        return_data = {}
+        for entry in data_all:
+            return_data[entry.key] = entry.value
+        return return_data
 
-class Intro2GetWithId(Resource):
+class Get(Resource):
     def get(self, data):
-        if not data in intro_dict:
-            return {'message': 'Data Key not found.'}
-        return {data: intro_dict[data]}
+        data_entry = Database.query.filter_by(key=data).first()
+        return {data_entry.key : data_entry.value}
 
-api.add_resource(Intro2Get, '/get')
-api.add_resource(Intro2GetWithId, '/get/<string:data>')
+api.add_resource(GetAll, '/get')
+api.add_resource(Get, '/get/<string:data>')
 
-class Intro2Post(Resource):
+class Post(Resource):
     def post(self, data):
-        if data in intro_dict:
-            return {'message': 'Data Key already exists.'}
-        intro_dict[data] = request.form[data]
-        return {data: intro_dict[data]}
+        parser = reqparse.RequestParser()
+        parser.add_argument(data)
+        args = parser.parse_args()
+        new_entry = Database(key = data, value = args[data])
+        db.session.add(new_entry)
+        db.session.commit()
+        return {data: args[data]}
 
-api.add_resource(Intro2Post, '/post/<string:data>')
+api.add_resource(Post, '/post/<string:data>')
 
-class Intro2Put(Resource):
+class Put(Resource):
     def put(self, data):
-        intro_dict[data] = request.form[data]
-        return {data: intro_dict[data]}
+        parser = reqparse.RequestParser()
+        parser.add_argument(data)
+        args = parser.parse_args()
+        data_entry = Database.query.filter_by(key=data).first()
+        data_entry.value = args['value']
+        db.session.commit()
+        return {data: args[data]}
         
-api.add_resource(Intro2Put, '/put/<string:data>')
+api.add_resource(Put, '/put/<string:data>')
 
 @app.route('/')
 def index():
-    return render_template('index.html', intro_dict=intro_dict)
+    return render_template('index.html', database=Database.query.all())
