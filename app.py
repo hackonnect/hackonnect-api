33404f7840c04f3f8ab85183624f6c2688b00a53
alienonolympus <alien-on-earth@outlook.com>
2019-01-12 15:07:51 +0800

    Added Post and Put requests

--- app.py
+++ app.py
@@ -1,22 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_restful import Resource, Api
 
 app = Flask(__name__)
 api = Api(app)
 
-intro2get = {'data1': 'value1', 'data2': 'value2', 'data3': 'value3'}
+intro_dict = {'data1': 'value1', 'data2': 'value2', 'data3': 'value3'}
 
 class Intro2Get(Resource):
     def get(self):
-        return intro2get
+        return intro_dict
 
 class Intro2GetWithId(Resource):
-    def get(self, data_name):
-        return {data_name: intro2get[data_name]}
+    def get(self, data)
+        return {data: intro_dict[data]}
 
 api.add_resource(Intro2Get, '/get')
-api.add_resource(Intro2GetWithId, '/get/<string:data_name>')
+api.add_resource(Intro2GetWithId, '/get/<string:data>')
+
+class Intro2Post(Resource):
+    def post(self, data):
+        if data in intro_dict:
+            return {'message': 'Data Key already exists.'}
+        intro_dict[data] = request.form['data']
+        return {data: intro_dict[data]}
+
+api.add_resource(Intro2Post, '/post/<string:data>')
+
+class Intro2Put(Resource):
+    def put(self, data):
+        intro_dict[data] = request.form['data']
+        return {data: intro_dict[data]}
+        
+api.add_resource(Intro2Put, '/put/<string:data>')
 
 @app.route('/')
 def index():
-    return render_template('index.html', intro2get=intro2get)
+    return render_template('index.html', intro_dict=intro_dict)
